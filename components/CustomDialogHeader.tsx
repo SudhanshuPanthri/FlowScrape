0b5d324faf966740e9616eaa42ff945d940c4456
sudhanshupanthri <panthrisudhanshu666@gmail.com>
2025-03-14 13:30:38 +0530

    Creating Workflow screen

---
+++ components/CustomDialogHeader.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { DialogHeader, DialogTitle } from "./ui/dialog";
+
+interface CustomDialogHeaderProps {
+  title?: string;
+  subTitle?: string;
+  icon?: React.ReactNode;
+
+  iconClassName?: string;
+  titleClassName?: string;
+  subTitleClassName?: string;
+}
+
+const CustomDialogHeader = ({
+  icon,
+  title,
+  subTitle,
+}: CustomDialogHeaderProps) => {
+  return (
+    <DialogHeader className="py-6">
+      <DialogTitle asChild>
+        <div className="flex flex-col items-center gap-2 mb-2"></div>
+      </DialogTitle>
+    </DialogHeader>
+  );
+};
+
+export default CustomDialogHeader;
