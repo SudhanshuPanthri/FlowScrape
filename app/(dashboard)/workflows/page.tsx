0b77055d33d297ab111c9e85e229f10d2128b28f
sudhanshupanthri <panthrisudhanshu666@gmail.com>
2025-03-17 12:27:39 +0530

    Adding Workflow Actions

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -5,6 +5,7 @@ import { waitFor } from "@/lib/helper/waitFor";
 import { AlertCircle, InboxIcon } from "lucide-react";
 import React, { Suspense } from "react";
 import CreateWorkflowDialog from "./_components/CreateWorkflowDialog";
+import WorkflowCard from "./_components/WorkflowCard";
 
 const Page = () => {
   return (
@@ -63,7 +64,13 @@ const UserWorkflows = async () => {
       </div>
     );
   }
-  return <pre>{JSON.stringify(workflows, null, 4)}</pre>;
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      {workflows.map((workflow) => (
+        <WorkflowCard key={workflow.id} workflow={workflow} />
+      ))}
+    </div>
+  );
 };
 
 export default Page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <AppProviders>{children}</AppProviders>
+          <Toaster richColors />
         </body>
-        <Toaster richColors />
       </html>
     </ClerkProvider>
   );
